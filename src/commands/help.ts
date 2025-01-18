a42356bc0e251354330804b6927cb3f4eaa209cd
thijnmens <thijnsmulders04@gmail.com>
2025-01-18 18:36:25 +0100

    Use correct prefix in help message

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,6 +1,8 @@
+import Config from '../config';
+
 export default function help(): string {
 	return (
-		'### Commands (prefix = `!!?`)\n' +
+		`### Commands (prefix = \`${Config.PREFIX}\`)` +
 		'| Command | Description |\n' +
 		'|---|---|\n' +
 		'| Help | Displays this page | \n' +
